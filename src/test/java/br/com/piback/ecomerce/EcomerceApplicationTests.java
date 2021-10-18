2db72eb70d87e4a3505c7e5c85caa4bf926da8f0
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-10-18 10:43:55 -0300

    configurando ambiente back-end

---
+++ src/test/java/br/com/piback/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piback.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
