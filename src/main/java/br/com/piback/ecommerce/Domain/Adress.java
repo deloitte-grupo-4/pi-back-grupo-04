3997f6a65a8fcf61e37127f54eb826e506db0fd7
mikaeldefs <mikaeldefreitas@hotmail.com>
2021-11-01 19:43:01 -0300

    G/S e Construtores consertados

--- src/main/java/br/com/piback/ecommerce/Domain/Adress.java
+++ src/main/java/br/com/piback/ecommerce/Domain/Adress.java
@@ -37,4 +37,89 @@ public class Adress {
     @Column(name = "cep")
     private String zipCode;
 
+    public Adress() {
+    }
+
+    public Adress(User user, String street, String number, String district, String complement, String city, String state, String zipCode) {
+        this.user = user;
+        this.street = street;
+        this.number = number;
+        this.district = district;
+        this.complement = complement;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
